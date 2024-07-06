cc528d5dd655e53216d54281bd150f524c26ac50
Kirill Uskov <kir.usckov@gmail.com>
2024-07-06 11:11:58 +0300

    add Colors Effects task

---
+++ ColorEffects/ColorEffects/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ColorEffects {
+    partial class ColorEffects_Form {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.ChoosePicture_Button = new System.Windows.Forms.Button();
+            this.NormalSizeMode_RadioButton = new System.Windows.Forms.RadioButton();
+            this.ZoomSizeMode_RadioButton = new System.Windows.Forms.RadioButton();
+            this.SizeMode_Label = new System.Windows.Forms.Label();
+            this.ColorEffect_Label = new System.Windows.Forms.Label();
+            this.ColorEffect_ComboBox = new System.Windows.Forms.ComboBox();
+            this.SaveImage_Button = new System.Windows.Forms.Button();
+            this.RotateClockwise_Button = new System.Windows.Forms.Button();
+            this.RotateCounterClockwise_Button = new System.Windows.Forms.Button();
+            this.Rotate_Label = new System.Windows.Forms.Label();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.Image_PictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BlurCheckBox = new System.Windows.Forms.CheckBox();
+            this.Panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Image_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChoosePicture_Button
+            // 
+            this.ChoosePicture_Button.Location = new System.Drawing.Point(874, 13);
+            this.ChoosePicture_Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ChoosePicture_Button.Name = "ChoosePicture_Button";
+            this.ChoosePicture_Button.Size = new System.Drawing.Size(181, 27);
+            this.ChoosePicture_Button.TabIndex = 0;
+            this.ChoosePicture_Button.Text = "Выбрать изображение";
+            this.ChoosePicture_Button.UseVisualStyleBackColor = true;
+            this.ChoosePicture_Button.Click += new System.EventHandler(this.ChoosePicture);
+            // 
+            // NormalSizeMode_RadioButton
+            // 
+            this.NormalSizeMode_RadioButton.AutoSize = true;
+            this.NormalSizeMode_RadioButton.Checked = true;
+            this.NormalSizeMode_RadioButton.Location = new System.Drawing.Point(875, 75);
+            this.NormalSizeMode_RadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NormalSizeMode_RadioButton.Name = "NormalSizeMode_RadioButton";
+            this.NormalSizeMode_RadioButton.Size = new System.Drawing.Size(144, 20);
+            this.NormalSizeMode_RadioButton.TabIndex = 2;
+            this.NormalSizeMode_RadioButton.TabStop = true;
+            this.NormalSizeMode_RadioButton.Text = "Исходный размер";
+            this.NormalSizeMode_RadioButton.UseVisualStyleBackColor = true;
+            this.NormalSizeMode_RadioButton.Click += new System.EventHandler(this.ChangeSizeMode);
+            // 
+            // ZoomSizeMode_RadioButton
+            // 
+            this.ZoomSizeMode_RadioButton.AutoSize = true;
+            this.ZoomSizeMode_RadioButton.Location = new System.Drawing.Point(875, 98);
+            this.ZoomSizeMode_RadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ZoomSizeMode_RadioButton.Name = "ZoomSizeMode_RadioButton";
+            this.ZoomSizeMode_RadioButton.Size = new System.Drawing.Size(140, 20);
+            this.ZoomSizeMode_RadioButton.TabIndex = 3;
+            this.ZoomSizeMode_RadioButton.Text = "Масштабировать";
+            this.ZoomSizeMode_RadioButton.UseVisualStyleBackColor = true;
+            this.ZoomSizeMode_RadioButton.Click += new System.EventHandler(this.ChangeSizeMode);
+            // 
+            // SizeMode_Label
+            // 
+            this.SizeMode_Label.AutoSize = true;
+            this.SizeMode_Label.Location = new System.Drawing.Point(871, 55);
+            this.SizeMode_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.SizeMode_Label.Name = "SizeMode_Label";
+            this.SizeMode_Label.Size = new System.Drawing.Size(140, 16);
+            this.SizeMode_Label.TabIndex = 4;
+            this.SizeMode_Label.Text = "Режим отображения";
+            // 
+            // ColorEffect_Label
+            // 
+            this.ColorEffect_Label.AutoSize = true;
+            this.ColorEffect_Label.Location = new System.Drawing.Point(872, 287);
+            this.ColorEffect_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.ColorEffect_Label.Name = "ColorEffect_Label";
+            this.ColorEffect_Label.Size = new System.Drawing.Size(39, 16);
+            this.ColorEffect_Label.TabIndex = 8;
+            this.ColorEffect_Label.Text = "Цвет";
+            // 
+            // ColorEffect_ComboBox
+            // 
+            this.ColorEffect_ComboBox.Enabled = false;
+            this.ColorEffect_ComboBox.FormattingEnabled = true;
+            this.ColorEffect_ComboBox.Items.AddRange(new object[] {
+            "Нет",
+            "Негатив"});
+            this.ColorEffect_ComboBox.Location = new System.Drawing.Point(874, 307);
+            this.ColorEffect_ComboBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ColorEffect_ComboBox.Name = "ColorEffect_ComboBox";
+            this.ColorEffect_ComboBox.Size = new System.Drawing.Size(180, 24);
+            this.ColorEffect_ComboBox.TabIndex = 9;
+            this.ColorEffect_ComboBox.SelectedIndexChanged += new System.EventHandler(this.ChangeColorEffect);
+            // 
+            // SaveImage_Button
+            // 
+            this.SaveImage_Button.Enabled = false;
+            this.SaveImage_Button.Location = new System.Drawing.Point(881, 430);
+            this.SaveImage_Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.SaveImage_Button.Name = "SaveImage_Button";
+            this.SaveImage_Button.Size = new System.Drawing.Size(183, 27);
+            this.SaveImage_Button.TabIndex = 10;
+            this.SaveImage_Button.Text = "Сохранить результат";
+            this.SaveImage_Button.UseVisualStyleBackColor = true;
+            this.SaveImage_Button.Click += new System.EventHandler(this.SaveResult);
+            // 
+            // RotateClockwise_Button
+            // 
+            this.RotateClockwise_Button.Enabled = false;
+            this.RotateClockwise_Button.Location = new System.Drawing.Point(875, 248);
+            this.RotateClockwise_Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RotateClockwise_Button.Name = "RotateClockwise_Button";
+            this.RotateClockwise_Button.Size = new System.Drawing.Size(181, 27);
+            this.RotateClockwise_Button.TabIndex = 14;
+            this.RotateClockwise_Button.Text = "По часовой";
+            this.RotateClockwise_Button.UseVisualStyleBackColor = true;
+            this.RotateClockwise_Button.Click += new System.EventHandler(this.RotateClockwise);
+            // 
+            // RotateCounterClockwise_Button
+            // 
+            this.RotateCounterClockwise_Button.Enabled = false;
+            this.RotateCounterClockwise_Button.Location = new System.Drawing.Point(875, 213);
+            this.RotateCounterClockwise_Button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RotateCounterClockwise_Button.Name = "RotateCounterClockwise_Button";
+            this.RotateCounterClockwise_Button.Size = new System.Drawing.Size(181, 27);
+            this.RotateCounterClockwise_Button.TabIndex = 15;
+            this.RotateCounterClockwise_Button.Text = "Против часовой";
+            this.RotateCounterClockwise_Button.UseVisualStyleBackColor = true;
+            this.RotateCounterClockwise_Button.Click += new System.EventHandler(this.RotateCounterClockwise);
+            // 
+            // Rotate_Label
+            // 
+            this.Rotate_Label.AutoSize = true;
+            this.Rotate_Label.Location = new System.Drawing.Point(871, 194);
+            this.Rotate_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Rotate_Label.Name = "Rotate_Label";
+            this.Rotate_Label.Size = new System.Drawing.Size(79, 16);
+            this.Rotate_Label.TabIndex = 16;
+            this.Rotate_Label.Text = "Повернуть";
+            // 
+            // Panel
+            // 
+            this.Panel.AutoScroll = true;
+            this.Panel.Controls.Add(this.Image_PictureBox);
+            this.Panel.Location = new System.Drawing.Point(9, 15);
+            this.Panel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(853, 443);
+            this.Panel.TabIndex = 13;
+            // 
+            // Image_PictureBox
+            // 
+            this.Image_PictureBox.Location = new System.Drawing.Point(0, 0);
+            this.Image_PictureBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Image_PictureBox.Name = "Image_PictureBox";
+            this.Image_PictureBox.Size = new System.Drawing.Size(640, 360);
+            this.Image_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Image_PictureBox.TabIndex = 1;
+            this.Image_PictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(874, 345);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Эффекты";
+            // 
+            // BlurCheckBox
+            // 
+            this.BlurCheckBox.AutoSize = true;
+            this.BlurCheckBox.Enabled = false;
+            this.BlurCheckBox.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BlurCheckBox.Location = new System.Drawing.Point(874, 365);
+            this.BlurCheckBox.Margin = new System.Windows.Forms.Padding(4);
+            this.BlurCheckBox.Name = "BlurCheckBox";
+            this.BlurCheckBox.Size = new System.Drawing.Size(91, 20);
+            this.BlurCheckBox.TabIndex = 18;
+            this.BlurCheckBox.Text = "Размытие";
+            this.BlurCheckBox.UseVisualStyleBackColor = true;
+            this.BlurCheckBox.CheckedChanged += new System.EventHandler(this.SetBlur);
+            // 
+            // ColorEffects_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1077, 470);
+            this.Controls.Add(this.BlurCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Rotate_Label);
+            this.Controls.Add(this.RotateCounterClockwise_Button);
+            this.Controls.Add(this.RotateClockwise_Button);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.SaveImage_Button);
+            this.Controls.Add(this.ColorEffect_ComboBox);
+            this.Controls.Add(this.ColorEffect_Label);
+            this.Controls.Add(this.SizeMode_Label);
+            this.Controls.Add(this.ZoomSizeMode_RadioButton);
+            this.Controls.Add(this.NormalSizeMode_RadioButton);
+            this.Controls.Add(this.ChoosePicture_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "ColorEffects_Form";
+            this.ShowIcon = false;
+            this.Text = "Цветовые эффекты";
+            this.Load += new System.EventHandler(this.ColorEffect_Form_Load);
+            this.Panel.ResumeLayout(false);
+            this.Panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Image_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChoosePicture_Button;
+        private System.Windows.Forms.RadioButton NormalSizeMode_RadioButton;
+        private System.Windows.Forms.RadioButton ZoomSizeMode_RadioButton;
+        private System.Windows.Forms.Label SizeMode_Label;
+        private System.Windows.Forms.Label ColorEffect_Label;
+        private System.Windows.Forms.ComboBox ColorEffect_ComboBox;
+        private System.Windows.Forms.Button SaveImage_Button;
+        private System.Windows.Forms.Button RotateClockwise_Button;
+        private System.Windows.Forms.Button RotateCounterClockwise_Button;
+        private System.Windows.Forms.Label Rotate_Label;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.PictureBox Image_PictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox BlurCheckBox;
+    }
+}
+
---
+++ ColorEffects/ColorEffects/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Accord.Imaging.Filters;
+
+namespace ColorEffects
+{
+    public partial class ColorEffects_Form : Form
+    {
+        // конфигурация окна для выбора файла
+        private OpenFileDialog OFD = new OpenFileDialog
+        {
+            Multiselect = false, // выбор только 1 файла
+            Filter = "Image files (*.jpg;*.png;*.gif;*.jpeg)|*.jpg;*.png;*.gif;*.jpeg", // доступные расширения
+            Title = "Выбрать изображение" // установка заголовка окна
+        };
+
+        // конфигурация окна для сохранения файла
+        private SaveFileDialog Save = new SaveFileDialog()
+        {
+            Filter = "JPG Image|*.jpg|PNG Image|*.png|GIF Image|*.gif",
+            Title = "Сохранить изображение"
+        };
+
+        // Изображение без эффектов для отмены действий
+        private Bitmap TempImagewWithoutEffect;
+        // Текущее изображение
+        private Bitmap TempImage;
+
+        public ColorEffects_Form()
+        {
+            // Вызов метода для инициализации элементов формы и установки им параметров
+            InitializeComponent();
+        }
+
+        // Метод, вызываемый при загрузке формы 
+        private void ColorEffect_Form_Load(object sender, EventArgs e)
+        {
+            // установка дефолтного значения в ComboBox
+            ColorEffect_ComboBox.SelectedIndex = 0;
+        }
+
+        // Метод, вызываемый кнопкой "Выбрать изображение"
+        private void ChoosePicture(object sender, EventArgs e)
+        {
+            // Если пользователь выбрал картинку и нажал "Открыть"
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                // Получение изображения из окна 
+                Image Img = Image.FromStream(OFD.OpenFile());
+                // Размещение изображения в буферы
+                TempImage = new Bitmap(Img); // текущий буфер
+                TempImagewWithoutEffect = (Bitmap)TempImage.Clone(); // буфер для отмены ч/б эффекта
+
+                // помещение изображения в PictureBox
+                Image_PictureBox.Image = new Bitmap(Img);
+                // Установка доступности для всех эффектов
+                ColorEffect_ComboBox.Enabled = true;
+                RotateClockwise_Button.Enabled = true;
+                RotateCounterClockwise_Button.Enabled = true;
+                SaveImage_Button.Enabled = true;
+                BlurCheckBox.Enabled = true;
+            }
+        }
+
+        // Метод вызывается при нажатии на один из RadioButton
+        private void ChangeSizeMode(object sender, EventArgs e)
+        {
+            // Если была нажата кнопка "Исходный размер"
+            if (sender == NormalSizeMode_RadioButton)
+            {
+                Image_PictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            }
+            // Если была нажата кнопка "Масштабирование"
+            else if (sender == ZoomSizeMode_RadioButton)
+            {
+                // Панель - внешний элемент, нужный для маштабирования в окне
+                Panel.VerticalScroll.Value = 0;
+                Panel.HorizontalScroll.Value = 0;
+                Image_PictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                Image_PictureBox.Width = Panel.Width;
+                Image_PictureBox.Height = Panel.Height;
+            }
+        }
+
+        // Метод, вызываемый при изменении значения в ComboBox "Цвет"
+        private void ChangeColorEffect(object sender, EventArgs e)
+        {
+            if (TempImage == null)
+                return;
+            switch (ColorEffect_ComboBox.SelectedIndex)
+            {
+                case 0:
+                    // установка изображения без эффектов в буфер 
+                    TempImage = (Bitmap)TempImagewWithoutEffect.Clone();
+                    // установка изображения без эффектов в PictureBox 
+                    Image_PictureBox.Image = (Bitmap)TempImage.Clone();
+                    break;
+                case 1:
+                    // функция для применения инверсии к картинке
+                    InvertColor();
+                    break;
+            }
+            // Примеенение блюра (нужно, т.к. мы используем TempImagewWithoutEffect)
+            Blur();
+            //Обнолвение картинки
+            Image_PictureBox.Invalidate();
+        }
+
+        // Функция дл яприменения инверсии цвета
+        private void InvertColor()
+        {
+            Bitmap BM = new Bitmap(TempImage);
+            // копирование картинки из TempImage в TempImagewWithoutEffect
+            TempImagewWithoutEffect = (Bitmap)BM.Clone();
+            int Height = BM.Height;
+            int Width = BM.Width;
+            // 2 вложенных цикла для прохождения по всем пикселям
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    // Получение пикселя по координатам
+                    Color p = BM.GetPixel(x, y);
+
+                    // альфа канал
+                    int a = p.A;
+                    // красный
+                    int r = p.R;
+                    // зеленый
+                    int g = p.G;
+                    // синий
+                    int b = p.B;
+
+                    // получение противоположного цвета для красного, зеленого и синего каналов
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
+
+                    // обновление пикселя по координатам с новыми знаечниями 
+                    BM.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+                }
+            }
+            // копирование в текущий буфер полученного инвертированного изобржения
+            TempImage = (Bitmap)BM.Clone();
+            // Размещение TempImage в PictureBox
+            Image_PictureBox.Image = TempImage;
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Против часовой"
+        private void RotateCounterClockwise(object sender, EventArgs e)
+        {
+            // Вращение против часовой
+            Rotate(sender, e, RotateFlipType.Rotate270FlipNone);
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "По часовой"
+        private void RotateClockwise(object sender, EventArgs e)
+        {
+            // Вращение по часовой
+            Rotate(sender, e, RotateFlipType.Rotate90FlipNone);
+        }
+
+        private void Rotate(object sender, EventArgs e, RotateFlipType rotateFlipType)
+        {
+            // Вращение текущего буфера
+            TempImage.RotateFlip(rotateFlipType);
+            // Вращение буфера без эффектов
+            TempImagewWithoutEffect.RotateFlip(rotateFlipType);
+            // если выбран стиль отображения "Исходный размер"
+            if (NormalSizeMode_RadioButton.Checked == true)
+            {
+                Image_PictureBox.Width = Image_PictureBox.Image.Width;
+                Image_PictureBox.Height = Image_PictureBox.Image.Height;
+            }
+            Blur();
+            Image_PictureBox.Invalidate();
+        }
+
+        // Метод, вызываемый при нажатии на кнопку "Сохранить результат"
+        private void SaveResult(object sender, EventArgs e)
+        {
+            // Открытие окна
+            Save.ShowDialog();
+            // Если ввели имя файла
+            if (Save.FileName != "")
+            {
+                // создание стрима для чтения/записи файла
+                System.IO.FileStream fs = (System.IO.FileStream)Save.OpenFile();
+                // получени из окна индекса выбранного типа файла
+                switch (Save.FilterIndex)
+                {
+                    case 1:
+                        this.Image_PictureBox.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        this.Image_PictureBox.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+
+                    case 3:
+                        this.Image_PictureBox.Image.Save(fs,
+                          System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                }
+                // закрытие потока
+                fs.Close();
+            }
+        }
+
+        // Метод, вызываемый во внутренних функциях
+        private void Blur()
+        {
+            // Если стоит галочка напротив чекбокса с блюром, то эффект применяетеся
+            if (BlurCheckBox.Checked)
+            {
+                ApplyGaussianBlur(1.2);
+            }
+            else
+            {
+                // Если галочки нет, то эффект снимается - текущеее изображение берется из буфера
+                Image_PictureBox.Image = TempImage;
+            }
+        }
+
+        // Метод, вызывающийся при нажатии на чекбокс Блюра
+        private void SetBlur(object sender, EventArgs e)
+        {
+            // Если в чекбоксе галочка
+            if (BlurCheckBox.Checked)
+            {
+                // применение блюра
+                ApplyGaussianBlur(1.2);
+            }
+            else
+            {
+                // Отмена блюра - высталвение в PictureBox картинки из буфера
+                Image_PictureBox.Image = TempImage;
+            }
+            //обнолвение картинки в PictureBox
+            Image_PictureBox.Invalidate();
+        }
+
+
+        // Метод для установки блюра
+        public void ApplyGaussianBlur(double sigma)
+        {
+            // Получение текущего изображения из буфера
+            Bitmap sourceImage = new Bitmap(TempImage);
+            // Создаем экземпляр фильтра размытия по Гауссу
+            GaussianBlur filter = new GaussianBlur(sigma);
+            // Применяем фильтр к изображению
+            Bitmap blurredImage = filter.Apply(sourceImage);
+            //Размещение получившегося изображения в PictureBox
+            Image_PictureBox.Image = blurredImage;
+        }
+    }
+}
